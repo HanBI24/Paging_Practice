ac533a0921b26601f557a7de8c3bd6ec231e05ae
HanBI24 <alsltjdrhs@naver.com>
2023-03-30 23:38:29 +0900

    Add Composable screen to HomeScreen

---
+++ Paging_library/app/src/main/java/com/example/paginglibrary/screens/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.paginglibrary.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+
+}
