9dff2ace8cdc517e6242829201e4c98166fe1d42
HanBI24 <alsltjdrhs@naver.com>
2023-04-01 01:30:46 +0900

    Add HomeViewModel

--- Paging_library/app/src/main/java/com/example/paginglibrary/screens/home/HomeScreen.kt
+++ Paging_library/app/src/main/java/com/example/paginglibrary/screens/home/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.ExperimentalPagingApi
+import androidx.paging.compose.collectAsLazyPagingItems
 import coil.annotation.ExperimentalCoilApi
 import com.example.paginglibrary.navigation.Screen
 import com.example.paginglibrary.screens.common.ListContent
@@ -19,7 +20,7 @@ fun HomeScreen(
     navController: NavHostController,
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
-    val getAllImages = homeViewModel.getAllImages.collectAsLazyPaingingItems()
+    val getAllImages = homeViewModel.getAllImages.collectAsLazyPagingItems()
 
     Scaffold(
         topBar = {
---
+++ Paging_library/app/src/main/java/com/example/paginglibrary/screens/home/HomeViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.paginglibrary.screens.home
+
+import androidx.lifecycle.ViewModel
+import androidx.paging.PagingData
+import com.example.paginglibrary.data.repository.Repository
+import com.example.paginglibrary.model.UnsplashImage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    repository: Repository
+) : ViewModel() {
+    val getAllImages = repository.getAllImages()
+}
